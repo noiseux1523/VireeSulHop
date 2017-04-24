f53834e78cfc7145d605db4d9c0331a761a0c0a8
Cedric Noiseux <cedric.noiseux15@gmail.com>
2017-04-23 21:06:00 -0400

    Added Biere.java + Changed xml files

---
+++ app/src/main/java/com/example/noiseux1523/vireesulhop/Biere.java
@@ -0,0 +1,15 @@
+package com.example.noiseux1523.vireesulhop;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Biere extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_biere);
+
+    }
+
+}
